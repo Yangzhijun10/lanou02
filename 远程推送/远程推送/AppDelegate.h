da6b49f02936d3ff941a92da4fbbe5471142780a
jay <jay@jay.local>
2015-08-14 10:04:07 +0800

    添加工程

---
+++ 远程推送/远程推送/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  远程推送
+//
+//  Created by jay on 15/8/14.
+//  Copyright (c) 2015年 jay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 远程推送/远程推送/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  远程推送
+//
+//  Created by jay on 15/8/14.
+//  Copyright (c) 2015年 jay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
